ec9400f5593b65f95fe4d577999954a7a7dd7923
Pintu Kumar Singh <85443972+pintukumarsingh29@users.noreply.github.com>
2021-07-31 22:22:13 +0530

    Optimized Bubble Sort

--- Array/bubble_sort.py
+++ Array/bubble_sort.py
@@ -1,11 +1,18 @@
 def bubble_sort(arr):
     n = len(arr)
-
+    flag = 0
     for i in range(n - 1):
-        for j in range(0, n - i - 1):
+        flag = 0
 
+        for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                flag = 1
+
+        if flag == 0:
+            print("Already Swapped")
+            break
+            
 
 
 arr = []
